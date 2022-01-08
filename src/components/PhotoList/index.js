b482075585c0873af5d8879cf25d4342a18d12c1
Keshia-Lee Martin <keshialeemartin@gmail.com>
2022-01-08 15:52:56 -0500

    completed adding modal feature and tested

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import Modal from "../Modal";
 
 const PhotoList = ({ category }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [ currentPhoto, setCurrentPhoto ] = useState();
+
   const [photos] = useState([
     {
       name: 'Grocery aisle',
@@ -100,18 +103,16 @@ const PhotoList = ({ category }) => {
     },
   ]);
 
-  const [isModalOpen, setIsModalOpen] = useState(false);
   const currentPhotos = photos.filter(photo => photo.category === category);
-  const [ currentPhoto, setCurrentPhoto ] = useState();
   
   const toggleModal = (image, i) => {
     setCurrentPhoto({...image, index: i});
-    setIsModalOpen(true);
+    setIsModalOpen(!isModalOpen);
   }
 
   return (
     <div>
-      {isModalOpen && <Modal currentPhoto={currentPhoto} />}
+      {isModalOpen && (<Modal currentPhoto={currentPhoto} onClose={toggleModal} />)}
       <div className="flex-row">
         {currentPhotos.map((image, i) => (
           <img
